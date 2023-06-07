b73694f9ac624872959fec949ad20d626d7b6c40
Monique Senna <119607046+MoniqueSenna@users.noreply.github.com>
2023-06-07 14:44:34 -0300

    commint inicial

---
+++ main.js
@@ -0,0 +1,19 @@
+  // Exemplo usando JavaScript puro
+  (function () {
+    'use strict'
+
+    // Captura o formulário quando for enviado
+    var forms = document.querySelectorAll('.needs-validation')
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
